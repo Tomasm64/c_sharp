ad5d8aac77c787a7616b11699c678297b33b1d65
Tomas Medina <tomas@evatech.net>
2024-03-10 12:17:50 -0400

    First commit

---
+++ alphabetize.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Bonjour");
+        string input = Console.ReadLine();
+        Console.WriteLine($"You wrote {input}");
+    }
+}
